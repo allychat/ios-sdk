f2b350fef9ac420657e8b843c7d5b4aaf0044aa1
Alexandr Turyev <alex.turyev@infotechrhythm.com>
2015-08-31 23:59:22 +0300

    no message

---
+++ AChatDemo/AChat.framework/Versions/A/Headers/ACRoomModel.h
@@ -0,0 +1,26 @@
+//
+//  ACRoomModel.h
+//  ACChat
+//
+//  Created by Alex on 6/18/15.
+//  Copyright (c) 2015 Octoberry. All rights reserved.
+//
+
+#import <AChat/ACBaseModel.h>
+#import <AChat/ACMessageModel.h>
+
+@interface ACRoomModel : ACBaseModel
+
+@property (nonatomic, strong) NSString *roomID;
+@property (nonatomic, strong) NSString *lastReadMessageID;
+@property (nonatomic, strong) NSString *firstMessageID;
+
+@property (nonatomic, assign, getter=isSupportRoom) BOOL supportRoom;
+
+@property (nonatomic, assign) NSUInteger unreadMessages;
+
+@property (nonatomic, strong) ACMessageModel *lastMessage;
+
+@property (nonatomic, strong) NSArray *users;//identifiers
+
+@end
---
+++ AChatDemo/AChat.framework/Versions/A/Headers/AChat.h
@@ -0,0 +1,18 @@
+//
+//  AChat.h
+//  AChat
+//
+//  Created by Alex on 6/22/15.
+//  Copyright (c) 2015 octoberry. All rights reserved.
+//
+
+#ifndef AChat_AChat_h
+#define AChat_AChat_h
+
+#import <AChat/ACEngine.h>
+#import <AChat/ACMessageModel.h>
+#import <AChat/ACRoomModel.h>
+#import <AChat/ACUserModel.h>
+#import <AChat/ACOperatorModel.h>
+
+#endif
